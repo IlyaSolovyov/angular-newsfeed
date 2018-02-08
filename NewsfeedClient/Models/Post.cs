a4a3f1a64866dccf29e177277236e2d5238a2ef6
IlyaSolovyov <solovyov909@gmail.com>
2018-02-08 23:21:39 +0200

    Added model prototypes for API mocking.

---
+++ NewsfeedClient/Models/Post.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NewsfeedClient.Models
+{
+    public class Post
+    {
+        public int Id { get; set; }
+        public string Category { get; set; }
+        public Source Source { get; set; }
+        public DateTime TimePosted { get; set; }
+        public string Author { get; set; }
+        public string Content { get; set; }
+        public Object[] Attachments { get; set; }
+    }
+}
---
+++ NewsfeedClient/Models/Source.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NewsfeedClient.Models
+{
+    public enum Source
+    {
+       VK,
+       Twitter
+    }
+}
