a3bb544e5d1b591159f2cc62becfc9c1037eb5b9
IlyaSolovyov <solovyov909@gmail.com>
2018-02-08 23:19:04 +0200

    Added Angular Material support.

--- NewsfeedClient/ClientApp/src/app/app.module.ts
+++ NewsfeedClient/ClientApp/src/app/app.module.ts
@@ -4,6 +4,8 @@ import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { RouterModule } from '@angular/router';
 
+import {MatButtonModule, MatCheckboxModule} from '@angular/material';
+
 import { AppComponent } from './app.component';
 import { NavMenuComponent } from './nav-menu/nav-menu.component';
 import { HomeComponent } from './home/home.component';
@@ -22,6 +24,8 @@ import { FetchDataComponent } from './fetch-data/fetch-data.component';
     BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
     HttpClientModule,
     FormsModule,
+    MatButtonModule,
+    MatCheckboxModule,
     RouterModule.forRoot([
       { path: '', component: HomeComponent, pathMatch: 'full' },
       { path: 'counter', component: CounterComponent },
