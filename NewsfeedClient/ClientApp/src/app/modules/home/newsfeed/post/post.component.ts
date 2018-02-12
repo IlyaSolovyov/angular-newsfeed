1f903cbec3874a044d760a517fae92c30e877454
IlyaSolovyov <solovyov909@gmail.com>
2018-02-12 16:43:56 +0200

    Added Post and PostList component stubs.

---
+++ NewsfeedClient/ClientApp/src/app/modules/home/newsfeed/post/post.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'newsfeed-post',
+    templateUrl: './post.component.html',
+    styleUrls: ['./post.component.css']
+})
+
+export class PostComponent {
+
+    constructor() {
+
+    }
+}
