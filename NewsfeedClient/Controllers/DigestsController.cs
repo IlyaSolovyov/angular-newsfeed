c53bc85336b12f9193b327fa9f9deb5fc24a0c04
IlyaSolovyov <solovyov909@gmail.com>
2018-03-01 20:09:42 +0200

    Added Digests controller with tested functionality. API (appears to be) completed.

--- NewsfeedClient/Controllers/DigestsController.cs
+++ NewsfeedClient/Controllers/DigestsController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using NewsfeedClient.DAL;
 using NewsfeedClient.Models;
 using NewsfeedClient.ViewModels;
@@ -45,8 +46,84 @@ namespace NewsfeedClient.Controllers
             {
                 return BadRequest("Something wrong with the database. Please try again in a moment.");
             }
-
+            db.Subscriptions.Add(new Subscription { DigestId = digest.Id, UserId = digest.CreatorId });
+            db.SaveChanges();
             return Ok("Digest " + digest.Name + "is successfully created.");
+        }
+
+        // POST: api/digests/5/sources
+        [HttpPost("{digestId}/sources")]
+        public IActionResult AddSource([FromForm]Source sourceData, int digestId)
+        {        
+            if (!db.Digests.Any(d => d.Id == digestId))
+            {
+                return NotFound("No such digest found in the database.");
+            }
+            Digest digest = db.Digests.FirstOrDefault(d => d.Id == digestId);
+
+            if (false) //unable to check without account system
+            {
+                return BadRequest("You're not able to add source to " + digest.Name + " because you are not its creator.");
+            }
+
+            if (!db.Sources.Any(s => s.Service==sourceData.Service && s.Url==sourceData.Url))
+            {
+                db.Sources.Add(sourceData);
+                db.SaveChanges();   
+            }
+            int sourceId = db.Sources
+                .FirstOrDefault(s => s.Service == sourceData.Service && s.Url == sourceData.Url)
+                .Id;
+
+            if (db.DigestSources.Any(ds => ds.DigestId == digestId && ds.SourceId == sourceId))
+            {
+                return BadRequest("This digest already contains this source.");
+            }
+
+            db.DigestSources.Add(new DigestSource { DigestId = digest.Id, SourceId = sourceId });
+            db.SaveChanges();
+            return Ok("Source " + sourceData.Name + " is successfully added to " + digest.Name + " digest.");
+        }
+
+        // DELETE: api/digests/5/sources/4
+        [HttpDelete("{digestId}/sources/{sourceId}")]
+        public IActionResult RemoveSource(int digestId, int sourceId)
+        {
+            if (!db.Digests.Any(d => d.Id == digestId))
+            {
+                return NotFound("No such digest found in the database.");
+            }
+
+            if (!db.Sources.Any(s => s.Id == sourceId))
+            {
+                return NotFound("No such source found in the database.");
+            }
+
+            if (false) //unable to check without account system
+            {
+                return BadRequest("You're not able to edit this digest because you are not its creator.");
+            }
+
+            if (!db.DigestSources.Any(ds => ds.DigestId == digestId && ds.SourceId == sourceId))
+            {
+                return BadRequest("This digest already doesn't contain this source.");
+            }
+
+           DigestSource digestSource = db.DigestSources
+                    .Include(ds => ds.Digest)
+                    .Include(ds => ds.Source)
+                    .FirstOrDefault(ds => ds.DigestId == digestId && ds.SourceId == sourceId);
+
+            db.Entry<DigestSource>(digestSource).State = Microsoft.EntityFrameworkCore.EntityState.Deleted;
+            db.SaveChanges();
+
+            if (!db.DigestSources.Any(s => s.SourceId==sourceId))
+            {
+                db.Entry<Source>(digestSource.Source).State = Microsoft.EntityFrameworkCore.EntityState.Deleted;
+                db.SaveChanges();
+            }
+
+            return Ok("Succesfully removed " + digestSource.Source.Name + " from " + digestSource.Digest.Name + ".");
         }
 
         // DELETE: api/digests/5
@@ -62,6 +139,11 @@ namespace NewsfeedClient.Controllers
             {
                 return NotFound("You're not able to delete digest " + digest.Name + "because you are not its creator.");
             }
+
+            db.Subscriptions.RemoveRange
+                (db.Subscriptions              
+                .Where(s => s.DigestId == digestId)
+                );
             db.Entry<Digest>(digest).State = Microsoft.EntityFrameworkCore.EntityState.Deleted;
             db.SaveChanges();
 
